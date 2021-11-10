383f8f8c29686ad23feacbd5bafc97d529a3ca20
RuslanMirov <ruslan777mirov@gmail.com>
2021-11-10 12:20:11 +0200

    Update addresses

--- src/constants/address/address.js
+++ src/constants/address/address.js
@@ -1,19 +1,19 @@
 // MAINNET
 
-export const AddressDepositor='0x6CeB3e93774f969a69fe13FD86389cBEF5403dc6'          // Fetch
-export const ClaimableAddress='0x1362984414A8B6985B6bbeaB03342A6fFF4de841'          // Stake
-export const DEXFormulaAddress='0x0518AeADb347a73a0200D2C3009e573D2417fd8A'         // Dex formula with pancake Router
-export const RouterAddress='0x10ED43C718714eb63d5aA57B78B54704E256024E'             // Pancake Roter
-export const UNDERLYING_TOKEN ='0x3791Ce6A40592F62009703F8776E2ae21ccF12D7'         // bCOT
-export const BUSDAddress='0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'               // BUSD address
-export const UNDERLYING_Pool='0xF54243252F569Eb4cAF55bF5d24A28D0acA73f2A'           // Pancake bCOT/WETH pool
+// export const AddressDepositor='0x6CeB3e93774f969a69fe13FD86389cBEF5403dc6'          // Fetch
+// export const ClaimableAddress='0x1362984414A8B6985B6bbeaB03342A6fFF4de841'          // Stake
+// export const DEXFormulaAddress='0x0518AeADb347a73a0200D2C3009e573D2417fd8A'         // Dex formula with pancake Router
+// export const RouterAddress='0x10ED43C718714eb63d5aA57B78B54704E256024E'             // Pancake Roter
+// export const UNDERLYING_TOKEN ='0x3791Ce6A40592F62009703F8776E2ae21ccF12D7'         // bCOT
+// export const BUSDAddress='0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'               // BUSD address
+// export const UNDERLYING_Pool='0xF54243252F569Eb4cAF55bF5d24A28D0acA73f2A'           // Pancake bCOT/WETH pool
 
 // TESTNET
 
-// export const AddressDepositor='0x6B3A8077A6C99DFB8E237ef9FDd282c3B721A095'
-// export const ClaimableAddress='0x18eA7B4EeB0b9Ac4Ef070856560124Ac215A7668'
-// export const DEXFormulaAddress='0x899F45Bd11FC2DEA7c85dD9a8bb1aceF886424c7'
-// export const RouterAddress='0xcCb6973225FE7e61B97E0fe1a7822EEc56884d92'
-// export const UNDERLYING_TOKEN ='0xD3B6933A448fF602711390f96E15c0B9cab5fF11'
-// export const BUSDAddress='0x44bdc3a48677e4814e338da0361c1fffe9037d18'
-// export const UNDERLYING_Pool='0xaEE2132c45971E51bb2D1a7B36DDc1251e8F1c55'
+export const AddressDepositor='0xBC8EcCEd821605c69e6FF33E94c37b0905deefB8'
+export const ClaimableAddress='0x0FC4c575999708Cc33FFE8cf3be08dd6Cf2bDf76'
+export const DEXFormulaAddress='0x73e982461370EB6E89aebEc687BA40524412610e'
+export const RouterAddress='0xcCb6973225FE7e61B97E0fe1a7822EEc56884d92'
+export const UNDERLYING_TOKEN ='0xf1B735685416253A8F7c8a6686970cA2B0cceCce'
+export const BUSDAddress='0xD3B6933A448fF602711390f96E15c0B9cab5fF11'
+export const UNDERLYING_Pool='0xB364CB08c700d95Bd2B554f4fBc69d5AAD724155'
