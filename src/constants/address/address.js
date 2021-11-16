e2145a995e045cafadf04a19fe194450784fcde4
RuslanMirov <ruslan777mirov@gmail.com>
2021-11-16 15:28:16 +0200

    Update addresses

--- src/constants/address/address.js
+++ src/constants/address/address.js
@@ -1,12 +1,12 @@
 // MAINNET
 
-export const AddressDepositor='0x6CeB3e93774f969a69fe13FD86389cBEF5403dc6'          // Fetch
-export const ClaimableAddress='0x1362984414A8B6985B6bbeaB03342A6fFF4de841'          // Stake
+export const AddressDepositor='0xe4610e05f8B742e9Ab1043149EF783371a587b04'          // Fetch
+export const ClaimableAddress='0x4b2D428b8F8bB2426C74caf612c1841A63907D0b'          // Stake
 export const DEXFormulaAddress='0x0518AeADb347a73a0200D2C3009e573D2417fd8A'         // Dex formula with pancake Router
 export const RouterAddress='0x10ED43C718714eb63d5aA57B78B54704E256024E'             // Pancake Roter
-export const UNDERLYING_TOKEN ='0x3791Ce6A40592F62009703F8776E2ae21ccF12D7'         // bCOT
+export const UNDERLYING_TOKEN ='0x29072A31eeB3Fe6A3088C44f1443f2a5b19daE2A'         // bCOT
 export const BUSDAddress='0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'               // BUSD address
-export const UNDERLYING_Pool='0xF54243252F569Eb4cAF55bF5d24A28D0acA73f2A'           // Pancake bCOT/WETH pool
+export const UNDERLYING_Pool='0x044473d9a2659d074d363c22f4A57840Cf3d3436'           // Pancake bCOT/WETH pool
 
 // // OLD MAINNET
 
